22e562e85e19560e886b7c7000b018def0500601
badrinathvm@gmail.com <badrinathvm@gmail.com>
2017-07-23 21:03:42 -0700

    Added find missing #

--- SearchRotatedArrays.playground/Contents.swift
+++ SearchRotatedArrays.playground/Contents.swift
@@ -85,7 +85,146 @@ var number1 = arr.filter { (value:Int) -> Bool in
 
 print(number1)
 
+print("Programm for pair in sorted rotated array")
+
+struct PairSortedArray{
+    
+    static func pairSortedArray(arr:[Int], n:Int, x:Int) -> Bool{
+        
+        var k :Int = 0
+        //Find teh pivot element
+        for i in 0..<n{
+            if (arr[i] > arr[i+1]){
+                k = i
+                break
+            }
+        }
+        
+        var l:Int = (k+1)%n
+        var r:Int = k
+        
+        
+        while (l != r){
+            
+            //If it equals sum
+            if ( (arr[l] + arr[r]) == x){
+                return true
+            }
+            
+                // If current pair sum is less, move to the higher sum
+            if ( (arr[l] + arr[r]) < x){
+                l = (l+1)%n
+            }else{
+                //Move to the lower sum side
+                r = (n+r-1)%n
+            }
+        }
+
+        return false
+    }
+    
+}
+
+var a:[Int] = [11,15,6,8,9,10]
+print(PairSortedArray.pairSortedArray(arr: a, n: a.count, x: 14))
 
 
+print("Program to segregate odd and even")
 
+struct SegregateOddEven{
+    
+    static func segregateOddEven(arr: inout [Int]) -> [Int]{
+        var (left,right) = (0,arr.count-1)
+        
+        while ( left < right ){
+            
+            //find even number and keep on incrementing
+            if ( arr[left]%2 == 0 && left < right){
+                left += 1
+            }
+            //find odd even number and decrementing
+            if(arr[right]%2 == 1 && left < right){
+                right -= 1
+            }
+            
+            if (left < right){
+                let temp = arr[left]
+                arr[left] = arr[right]
+                arr[right] = temp
+                left += 1
+                right -= 1
+            }
+        }
+        
+        return arr
+    }
+    
+    static func segregateZeroOne(arr: inout [Int]) -> [Int]{
+        var (left,right) = (0,arr.count-1)
+        
+        while ( left < right ){
+            
+            //find even number and keep on incrementing
+            if ( arr[left]%2 == 0 && left < right){
+                left += 1
+            }
+            //find odd even number and decrementing
+            if(arr[right]%2 == 1 && left < right){
+                right -= 1
+            }
+            
+            if (left < right){
+                arr[left] = 0
+                arr[right] = 1
+                left += 1
+                right -= 1
+            }
+        }
+        
+        return arr
+    }
+
+}
+
+var oddEvenArray = [1,2,3,4,5,6,7]
+print(SegregateOddEven.segregateOddEven(arr: &oddEvenArray))
+
+var zeroOneArray = [1,0,0,1,1,0,0]
+print(SegregateOddEven.segregateZeroOne(arr: &zeroOneArray))
+
+
+print("Find the first smallest number")
+
+struct FindFirstSmallest{
+    
+    static func findFirstSmallest(nums: inout [Int]) -> Int?{
+        
+        var k:Int = 0
+        print(nums.count)
+        for i in 0..<nums.count{
+            if ( nums[i] > 0 && nums[i] <= nums.count){
+                var temp:Int = nums[i]
+                if( (nums[i]-1) != i && nums[temp-1] != nums[i]){
+                    print("Swapp")
+                    var t:Int = nums[temp-1]
+                    nums[temp-1] = nums[i]
+                    nums[i] = t
+                                k = i
+                    k -= 1
+                }
+            }
+        }
+        
+        for i in 0..<nums.count{
+            if (nums[i] != i+1){
+                return i+1
+            }
+        }
+        
+        return nums.count+1
+    }
+}
+
+var findArray = [3,4,-1,1]
+print(FindFirstSmallest.findFirstSmallest(nums: &findArray)!)
 
