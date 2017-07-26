4c38e65c2ce35b8d5124e14eb2c966739df96d0f
badrinathvm@gmail.com <badrinathvm@gmail.com>
2017-07-26 10:35:24 -0700

    added from few more tests

--- ComplexString.playground/Contents.swift
+++ ComplexString.playground/Contents.swift
@@ -13,15 +13,14 @@ private extension String{
 //var x:String = "ABCDGH"
 //var y:String = "AEDFHR"
 
-var x:String = "AGGTAB"
-var y:String = "GXTXAY"
+//var x:String = "AGGTAB"
+//var y:String = "GXTXAY"
 
-var m:Int = x.characters.count
-var n:Int = y.characters.count
+
 
 //creating a two dimensio array
 
-func lcs(m:Int, n:Int) -> Int{
+func lcs(m:Int, n:Int, x:String ,y:String) -> Int{
     
 var dp = Array(repeating: Array(repeatElement(0, count: n + 1)), count: m + 1)
     for i in 1...m {
@@ -38,7 +37,21 @@ var dp = Array(repeating: Array(repeatElement(0, count: n + 1)), count: m + 1)
     return dp[m][n]
 }
 
-print(lcs(m: m, n: n))
+print("Longest Common Substring or Subsequence")
+var x = "abcdxyz"
+var y = "xyzabcd"
+
+print(lcs(m: x.characters.count, n:y.characters.count, x:x, y:y))
+print()
+
+
+print("Minimum no of characters to make a palindrome")
+
+var str1 = "abcde"
+var str2 = String(str1.characters.reversed())
+print((str1.characters.count - lcs(m: str1.characters.count, n:str2.characters.count, x:str1, y:str2)))
+print()
+
 
 
 print("Length of the longest sub string is the forming the string wihtout repeatation")
@@ -73,10 +86,53 @@ struct LengthOfLongestSubString{
     
 }
 
-//var s = "ABDEFGABEF"
-var s = "GEEKSFORGEEKS"
+var s = "ABDEFGABEF"
+//var s = "GEEKSFORGEEKS"
 
 print(LengthOfLongestSubString.lengthofLongestSubString(str:s))
+
+
+print("Programme to eliminate adjacent duplicates")
+
+struct EliminateAdjacentDuplicates{
+    
+    static func eliminateDuplicates(str: inout String, n:Int) ->String{
+        
+        var result:String = ""
+        var (k,len,i) = (0,str.characters.count,0)
+        
+        var chars = Array(str.characters)
+        while(i<len){
+            
+            //if no match copy previous characarter
+            if( chars[i-1] != chars[i]){
+               chars[k] = chars[i-1]
+                k += 1
+            }else{
+                //if match happens skip character
+                while( chars[i-1] == chars[i]){
+                    i += 1
+                }
+            }
+            i += 1
+        }
+        
+        chars[k] = chars[i-1]
+        
+        if(k != n){
+            eliminateDuplicates(str: &str, n: k)
+        }else{
+            result =  String(chars)
+        }
+        
+        return result
+    }
+    
+}
+
+var s1 = "geeksforgeek"
+
+//print(EliminateAdjacentDuplicates.eliminateDuplicates( str:&s, n:s.characters.count ))
 
 
 
