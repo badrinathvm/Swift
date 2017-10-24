1df3f19c2bd24aaab1d3735bee8df61f917dd648
Badarinath Venkatnarayansetty <badarinath.venkatnarayansetty@macys.com>
2017-10-23 18:31:04 -0700

    Added few more programmes

--- Master3.playground/Contents.swift
+++ Master3.playground/Contents.swift
@@ -9,6 +9,8 @@ var str = "Hello, playground"
 
 var arr = [1.0, 4, 2.0, 2.0, 6, 24, 15, 2, 60, 15, 6]
 
+
+
 var arr1 = [1, 4 ,6, 24, 15 ]
 
 extension Array where Element : Hashable{
@@ -197,6 +199,283 @@ let arraofOptionals : [Int?] = [ nil,2,3,nil,4]
 for case let number? in arraofOptionals{
     print("Founder a \(number)")
 }
+
+var dict:[String: Int] = ["one":1,"two":0]
+
+let keys = dict.flatMap {  $0.0 }
+print(keys)
+
+let values = dict.flatMap { $0.1 }
+print(values)
+
+let filtered = dict.filter{ $0.1 > 0 }
+filtered.map({ (key: String, value: Int) -> Bool in
+    print("\(key) \(value)")
+    return true
+})
+
+let oddArray = [ 1,2,4,3,5,7]
+
+let resultOddArray = oddArray.filter({ ($0 % 2 == 1) })
+
+print(resultOddArray)
+
+
+var unSortMap:[String:Int] = ["t":12,"z":10, "a":1]
+print(unSortMap.sorted { $0.0 < $1.0 })
+print(unSortMap.sorted { $0.1 < $1.1 })
+
+for i in unSortMap.sorted(by:{ $0.0 < $1.0 } ){
+    print ("\(i.key) ---> \(i.value)" )
+}
+
+//Swift 4 Solution
+
+/* var singleNumberArray = [1, 2, 2, 3, 3, 4, 4, 4,5]
+var count:[Int:Int] = [:]
+singleNumberArray.forEach { count[$0,default:0] += 1 }
+var result = count.filter { $0.1 == 1 }.flatMap { $0.0 }
+print(result ) 
+ 
+ 
+ //Frequency Sort
+ 
+ var frequencyMap = [Int:Int]()
+ singleNumberArray.forEach { frequencyMap[$0,default:0] += 1 }
+ frequencyMap.sorted (by: { $0.1 < $1.1 }).forEach{ result in
+ print( result.key )
+ }
+ */
+
+
+var majorityArray = [3,3,3,3,3,10,10,1,2]
+majorityArray.sort()
+print(majorityArray[majorityArray.count/2])
+
+
+//First unique Character
+
+//var uniqueCharacter = "leetcode"
+//var resultDict = [Character:Int]()
+//var strArray = uniqueCharacter.characters.flatMap { $0 }
+//
+//strArray.forEach{
+//    resultDict[$0,default:0] += 1
+//}
+//
+//for ch in strArray{
+//    print((resultDict.filter { ($0.0 == ch ) && ($0.1 == 1) }.first?.key)! as Any )
+//    break
+//}
+
+
+import Foundation
+
+class ReverseVowels{
+    
+    func reverseVowels(str: inout String){
+        var vowList = Array<String>()
+        vowList.append("a")
+        vowList.append("e")
+        vowList.append("i")
+        vowList.append("o")
+        vowList.append("u")
+        
+        var strArray = str.characters.flatMap { String($0) }
+        var ( i, j) = (0, strArray.count-1)
+        
+        while( i < j ){
+            
+            if( !vowList.contains( strArray[i]) ){
+                i += 1
+                continue
+            }
+            
+            if( !vowList.contains( strArray[j])){
+                j -= 1
+                continue
+            }
+            
+            //strArray.swapAt(i,j)
+            swap(&strArray[i], &strArray[j])
+            i += 1
+            j -= 1
+        }
+        
+        
+        //print(String(strArray))
+        
+        print(strArray.joined(separator: ""))
+        
+    }
+    
+}
+
+
+var sz = "hello"
+
+ReverseVowels().reverseVowels(str: &sz)
+
+print(sz.characters.sorted())
+
+
+class Panagrams{
+    
+    /* function to detect panagrams */
+    
+    func detectPanagrams(){
+        var str = "The quick brown fox jumps over a lazy dog."
+        
+        str = str.trimmingCharacters(in: .whitespaces)
+            .replacingOccurrences(of: " ", with: "")
+            .replacingOccurrences(of: ".", with: "")
+        
+        var dict = [Character:Int]()
+        
+        str.forEach {
+            if(!dict.keys.contains($0) ){
+                dict[$0 ,default:0] += 1
+            }
+        }
+        
+        let values = dict.flatMap { $0.1 }.reduce(0, {x,y in x+y})
+        let result = (values == 26 ) ? "Panagrams" : "Not Panagrams"
+        print(result)
+    }
+    
+    
+    /* Function for Max Occuring */
+    
+    func maxOccuring(){
+        var str = "Hello! Are you all fine? What are u doing today? Hey Guyz,Listen! I  have a plan for today. Help!"
+        
+        str = str.replacingOccurrences(of:" ", with:"")
+        
+        var countDict = [Character:Int]()
+        
+        str.forEach{
+            countDict[$0, default:0] += 1
+        }
+        
+        //fetching the maximum count from dictionary
+        let maxCount = countDict.max { $0.1 < $1.1 }?.value
+        //print(maxCount)
+        
+        //sorting the dictionary and filtering the max time occuring ones.
+        let resultDict = countDict.sorted { $0.1 > $1.1 }
+            .filter { $0.1 == maxCount! }
+        
+        print(resultDict.flatMap { $0.0 }.sorted() )
+        
+    }
+    
+    
+    //function to find Uniquness or not
+    
+    func unique(){
+        
+        let str = "ABCC"
+        
+        var uniqDict = [Character: Int] ()
+        
+        str.forEach{
+            uniqDict[$0, default:0] += 1
+        }
+        
+        
+        let result = uniqDict.filter { $0.1 > 1 }.count >= 1 ? "Not Unique" : "Unique"
+        
+        print(result)
+        
+    }
+    
+    
+    func eliminateDuplicates(){
+        
+        let str = "aaabbbcc"
+        var removeDuplicatesDict = [Character:Int]()
+        var resultString = ""
+        
+        str.forEach{
+            if(!removeDuplicatesDict.keys.contains( $0 ) ){
+                resultString += String($0)
+                removeDuplicatesDict[$0, default:0] += 1
+            }
+        }
+        
+        print(resultString)
+        
+        //print(removeDuplicatesDict.flatMap { $0.0}.reduce("", { String($0) + String($1) }))
+        
+    }
+    
+    
+    //function to reverse words
+    
+    func reverseWords(){
+        var str = " Hello how are you"
+        
+        str = str.trimmingCharacters(in: .whitespaces)
+        
+        let strArray = str.components(separatedBy: " ")
+        
+        for word in strArray.reversed(){
+            print(word, terminator: "\t")
+        }
+        
+        print()
+        
+    }
+}
+
+
+let panagrams = Panagrams()
+panagrams.detectPanagrams()
+panagrams.maxOccuring()
+panagrams.unique()
+panagrams.eliminateDuplicates()
+panagrams.reverseWords()
+
+
+//let str = "abcdefg"
+//let indexStartOfText = str.index(str.startIndex, offsetBy: 3)
+//let indexEndOfText = str.index(str.endIndex, offsetBy: -1)
+//print(str[indexStartOfText..<indexEndOfText])
+
+
+
+// print(str[2...])
+
+// let greeting = "Hi there! It's nice to meet you!"
+// let endOfSentence = greeting.index(of: "!")!
+// let firstSentence = greeting[...endOfSentence]
+// print(firstSentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
