b3aa2a4abd2ad1a682044d8b328fdd6ae681f13c
Badarinath Venkatnarayansetty <badarinath.venkatnarayansetty@macys.com>
2017-10-28 08:31:28 -0700

    Added rotation array problems

--- ArrayProblems.playground/Contents.swift
+++ ArrayProblems.playground/Contents.swift
@@ -101,7 +101,7 @@ class ArrayProblems{
         let x = 14
         var isFound = false
         
-        arr.enumerated().map{ (i,element)  in
+        arr.enumerated().forEach{ (i,element)  in
             for j in 1..<arr.count-i{
                 if( arr[i] + arr[j] == x){
                     isFound = true
@@ -110,6 +110,84 @@ class ArrayProblems{
             }
         }
         print( isFound ? true: false )
+    }
+    
+    func productArray(){
+        
+        let arr = [10,3,5,6,2] //[180,600,360,300,900]
+        
+        
+        // for i in 0..<arr.count{
+        //   print(arr.filter { $0 != arr[i] }.reduce(1, {x,y in x*y}))
+        // }
+        
+        let result = arr.map{ val in
+            arr.filter { $0 != val }.reduce(1, {x,y in x*y})
+        }
+        
+        print(result)
+        
+    }
+    
+    func rotateRight(k: Int){
+        let arr = [1,2,3,4,5,6,7]
+        var reversedArray = Array(arr.reversed())
+        let rotations = (arr.count + k % arr.count) % arr.count
+        print(rotations)
+        
+        let leftPart = Array (reversedArray[0..<rotations].reversed())
+        let rightPart = Array(reversedArray[rotations..<arr.count].reversed())
+        
+        print( leftPart + rightPart )
+        
+    }
+    
+    func rotateByLeft(k: Int){
+        var leftRotate = [1,2,3,4,5,6,7]
+        for _ in 0..<k {
+            let first = leftRotate.removeFirst()
+            leftRotate.insert(first,at:leftRotate.count)
+        }
+        print(leftRotate)
+    }
+    
+    func searchInRotatedArray(){
+        let arr = [3,5,1,2,4]
+        let key = 4
+        
+        var resultArray = [Int]()
+        let result = arr.enumerated().map { (index,element) -> [Int] in
+            if(element == key){
+                resultArray.append(index)
+            }
+            
+            return  resultArray
+        }
+        
+        print("Found at Index \((result.last?.first)!) ")
+        
+    }
+    
+    func findMaximumValueOnRotations(){
+        var arr = [10, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        arr.forEach{ element in
+            
+            let last = (arr.suffix(1).first)!
+            
+            // rotate left until largest element is present at the end.
+            if(!(last > element)) {
+                let first = arr.removeFirst()
+                arr.insert(first,at:arr.count)
+            }
+        }
+        
+        let result =  arr.enumerated().map{ (index,value) in
+            return index * value
+        }
+        
+        print(result.reduce (0, { $0 + $1 }) )
+        
     }
     
 }
@@ -121,5 +199,37 @@ arrayProblems.sortArrayAccordingToAbsoluteDifference()
 arrayProblems.segregateOddEven()
 arrayProblems.largestPairInUnsortedArray()
 arrayProblems.pairSortedArray()
+arrayProblems.productArray()
+arrayProblems.rotateRight(k:3)
+arrayProblems.rotateByLeft(k:2)
+arrayProblems.searchInRotatedArray()
+arrayProblems.findMaximumValueOnRotations()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
