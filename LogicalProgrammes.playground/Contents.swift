307ab00b053ca715b1992b51d62c81f55ead5044
badrinathvm@gmail.com <badrinathvm@gmail.com>
2017-11-02 21:53:33 -0700

    Added 3 sum programme

--- LogicalProgrammes.playground/Contents.swift
+++ LogicalProgrammes.playground/Contents.swift
@@ -70,6 +70,51 @@ class Logical{
         
         print(arr)
     }
+    
+    
+    func threeSum(nums: [Int]) -> [[Int]] {
+        var nums = nums.sorted(by: <)
+        var res = [[Int]]()
+        
+        if nums.count <= 2 {
+            return res
+        }
+        
+        for i in 0...nums.count - 3 {
+            if i == 0 || nums[i] != nums[i - 1] {
+                let remain = -nums[i]
+                var left = i + 1
+                var right = nums.count - 1
+                while left < right {
+                    if nums[left] + nums[right] == remain {
+                        var temp = [Int]()
+                        temp.append(nums[i])
+                        temp.append(nums[left])
+                        temp.append(nums[right])
+                        
+                        res.append(temp)
+                        repeat {
+                            left += 1
+                        } while (left < right && nums[left] == nums[left - 1])
+                        repeat {
+                            right -= 1
+                        } while (left < right && nums[right] == nums[right + 1])
+                    } else if nums[left] + nums[right] < remain {
+                        repeat {
+                            left += 1
+                        } while (left < right && nums[left] == nums[left - 1])
+                    } else {
+                        repeat {
+                            right -= 1
+                        } while (left < right && nums[right] == nums[right + 1])
+                    }
+                }
+            }
+        }
+        
+        return res
+    }
+
 
     
 }
@@ -78,4 +123,5 @@ var logical = Logical()
 logical.twoSum()
 logical.stringCompress()
 logical.plusOne()
+print(logical.threeSum(nums: [-1,0,1,2,-1,-4]))
 
