f1782a3f9efb498eb5ccaef01046a2c912051a54
badrinathvm@gmail.com <badrinathvm@gmail.com>
2017-11-02 21:43:03 -0700

    Added plus one and formatted the code

--- LogicalProgrammes.playground/Contents.swift
+++ LogicalProgrammes.playground/Contents.swift
@@ -2,57 +2,80 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-
-func twoSum() -> [Int]{
+class Logical{
     
-    let twoSumArray = [2,1,4,1]
-    let target = 5
-    var dict  = [Int:Int]()
-    
-    for (index,element) in twoSumArray.enumerated()  {
+    func twoSum() -> [Int]{
         
-        //print("Element is \(dict[target-element])")
+        let twoSumArray = [2,1,4,1]
+        let target = 5
+        var dict  = [Int:Int]()
         
-        if let lastIndex = dict[target-element]{
-            print(lastIndex)
-            print(index)
-            return [lastIndex,index]
+        for (index,element) in twoSumArray.enumerated()  {
+            
+            //print("Element is \(dict[target-element])")
+            
+            if let lastIndex = dict[target-element]{
+                print(lastIndex)
+                print(index)
+                return [lastIndex,index]
+            }
+            
+            dict[element] = index
         }
         
-        dict[element] = index
+        fatalError("No valid Inputs")
+        
+        //print(dict)
     }
     
-    fatalError("No valid Inputs")
     
-    //print(dict)
-}
-
-twoSum()
-
-
-func stringCompress() {
-    //updated chunks array is passed to next iteration
-    
-    let chunks = str.characters.reduce([(Character, Int)]()) { (chunks, char) -> [(Character, Int)] in
+    func stringCompress() {
         
+        var str = "aaabccdd"
+        //updated chunks array is passed to next iteration
         
-        //get a mutable copy
-        var _chunks = chunks
+        let chunks = str.characters.reduce([(Character, Int)]()) { (chunks, char) -> [(Character, Int)] in
+            
+            //get a mutable copy
+            var _chunks = chunks
+            
+            //get a count if last char is same; 0 otherwise
+            let count = _chunks.last?.0 == char ? _chunks.last!.1 : 0
+            //remove tuple inorder order to update the latest count one.
+            if count > 0 {_chunks.removeLast()}
+            
+            //append tuple with incremented count
+            _chunks.append((char, count + 1))
+            return _chunks
+        }
         
-        //get a count if last char is same; 0 otherwise
-        let count = _chunks.last?.0 == char ? _chunks.last!.1 : 0
-        //remove tuple inorder order to update the latest count one.
-        if count > 0 {_chunks.removeLast()}
-        
-        //append tuple with incremented count
-        _chunks.append((char, count + 1))
-        return _chunks
+        print(chunks.map { chunk in  chunk.1 == 1 ? "\(chunk.0)" : "\(chunk.0)\(chunk.1)" }.reduce("",{ String($0) + String($1) }))
     }
     
-    print(chunks.map { chunk in  chunk.1 == 1 ? "\(chunk.0)" : "\(chunk.0)\(chunk.1)" }.reduce("",{ String($0) + String($1) }))
+    func plusOne(){
+        var arr = [9,9]
+        var index = arr.count - 1
+        
+        while ( index >= 0){
+            if( arr[index] < 9){
+                arr[index] += 1
+                return
+            }
+            
+            arr[index] = 0
+            index -= 1
+        }
+        
+        arr.insert(1,at:0)
+        
+        print(arr)
+    }
+
     
 }
 
-stringCompress()
+var logical = Logical()
+logical.twoSum()
+logical.stringCompress()
+logical.plusOne()
+
