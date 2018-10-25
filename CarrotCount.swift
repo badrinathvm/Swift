7ffa42704d5a377dace7c213f1a379d25b0ce4c2
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2018-10-25 08:03:15 -0700

    Refactoring

--- CarrotCount.swift
+++ CarrotCount.swift
@@ -1,119 +1,137 @@
-// This is a sandbox to experiment with CoderPad's execution capabilities.
-// It's a temporary, throw-away session only visible to you.
 
-import Foundation
-
-//  # 1. Find center, or the highest number closest to it 
-//   # 2. Set carrot_count to 0, increment count accordingly 
-//   # 3. Find the square next to it with the highest number 
-//   # 4. Repeat step 2 until it there are no adjacent squares other than 0's
-//   # 5. Break loop, return carrot_count 
-
-class CarrotSearch {
-  
-  var arr:[[Int]] 
-  var currentRow = 0
-  var currentColumn = 0
-  var rabbitSleep = false
-  var carrotCount = 0 
-  
-   init(arr: [[Int]]) {
-     self.arr = arr
-   }
-  
-  //Find the possible center or the highest number closer to it
-  func findPossibleCenters(size: Int) -> [Int] {
-    if size%2 == 0 {
-      return [size/2 - 1 ,size/2 ]
-    }else {
-      return [size/2 ]
+/*  
+ 1. Find the center or highest number closet to it.
+ 2. Set the carrot count to zero initially and increment it.
+ 3. Find the square next to it with highest number ( made use of dictionary to dump [key, (row,column)] extract the key with highest value.
+ 4. Repeat step 2 until it there are no adjacent squares other than 0's
+ 5. Break loop, return carrot count */
+ 
+ class CarrotSearch {
+    
+    var squareMatrix:[[Int]]
+    var currentRow = 0
+    var currentColumn = 0
+    var rabbitSleep = false
+    var carrotCount = 0
+    
+    init(matrix: [[Int]]) {
+        self.squareMatrix = matrix
     }
-  }
-  
-  func determineCenterPosition(rows: [Int], columns: [Int]) {
-    var dict:[Int:[Int]] = [:]
-    rows.forEach { row in
-      columns.forEach { column in 
-        let element = arr[row][column]
-        dict[element] = [row,column]
-      }
+    
+    //returns the closet ones if size is even, middle indices if the size is odd.
+    func findPossibleCenters(size: Int) -> [Int] {
+        if size%2 == 0 {
+            return [size/2 - 1 ,size/2 ]
+        }else {
+            return [size/2 ]
+        }
     }
-
-    highestValues(dict)    
-    print("current Row is \(currentRow)")
-    print("current Column is \(currentColumn)")
-
-  }
-  
-  func eatCarrot() {
-    let square = arr[currentRow][currentColumn]
-    if square == 0 {
-      rabbitSleep = true
-      return
+    
+    //Determines possible start positions, dump them in to dictionary to highest key's row and column.
+    func determineCenterPosition(rows: [Int], columns: [Int]) {
+        var dict:[Int:[Int]] = [:]
+        
+        //dump the row colums for the rows and columns of possible centers
+        rows.forEach { row in
+            columns.forEach { column in
+                let element = squareMatrix[row][column]
+                dict[element] = [row,column]
+            }
+        }
+        
+        //Fetch the highest key's row and column values
+        highestValues(dict)
     }
-      carrotCount += square
-      arr[currentRow][currentColumn] = 0
-  }
-  
-  func possibleMoves() {
-    let row = currentRow
-    let column = currentColumn
-    var dict:[Int: [Int]] = [:]
-
-    //move up, down,left,right
-    if row + 1 <= arr.count {
-      let element = arr[row + 1][column]
-      dict[element] = [row+1, column]
+    
+    func highestValues(_ dict : [Int: [Int]]) {
+        //sort based on the higehest key
+        let sortedDict = dict.sorted { $0.0 < $1.0 }
+        
+        guard let values = sortedDict.last?.value else {
+           print("Unable to get last value")
+           return
+        }
+        
+        currentRow = values.first!
+        currentColumn = values.last!
     }
-
-    if row - 1 >= 0  {
-      let element = arr[row - 1][column]
-      dict[element] = [row - 1, column]
+    
+    // 1.Increments the carrot count,
+    // 2.set value of the square to zero,
+    // 3.If rabbit can move to other 0-squares , fall asleep.
+    func eatCarrot() {
+        let square = squareMatrix[currentRow][currentColumn]
+        if square == 0 {
+            rabbitSleep = true
+            return
+        }
+        carrotCount += square
+        squareMatrix[currentRow][currentColumn] = 0
     }
-
-    if column + 1 <= arr[0].count {
-      let element = arr[row][column + 1]
-      dict[element] = [row, column + 1]
+    
+    //1. Calculate the all possible movies of rabbit up/down/left/right,
+    //2. Dump in dictionary for each move and then fid the highest of it.
+    func possibleMovements() {
+        let row = currentRow
+        let column = currentColumn
+        var dict:[Int: [Int]] = [:]
+        
+        //move to next row
+        if row + 1 <= squareMatrix.count {
+            let element = squareMatrix[row + 1][column]
+            dict[element] = [row+1, column]
+        }
+        
+        //move to row behind ( previous )
+        if row - 1 >= 0  {
+            let element = squareMatrix[row - 1][column]
+            dict[element] = [row - 1, column]
+        }
+        
+        //move to next column
+        if column + 1 <= squareMatrix[0].count {
+            let element = squareMatrix[row][column + 1]
+            dict[element] = [row, column + 1]
+        }
+        
+        //move to previous column
+        if column - 1 >= 0  {
+            let element = squareMatrix[row][column - 1]
+            dict[element] = [row , column - 1]
+        }
+        
+        // Fetch the highest key's row and column values
+        highestValues(dict)
     }
-
-    if column - 1 >= 0  {
-       let element = arr[row][column - 1]
-      dict[element] = [row , column - 1]
+    
+    func fetchCarrotCount() -> Int {
+        //Identify the possible centers based on the size of the matrix
+        let startRows = findPossibleCenters(size: squareMatrix.count)
+        let startColumns = findPossibleCenters(size: squareMatrix[0].count)
+        
+        determineCenterPosition(rows: startRows, columns: startColumns)
+        
+        //continue as long as the rabbit is asleep.
+        while !rabbitSleep{
+            eatCarrot()
+            possibleMovements()
+        }
+        
+        print("Carrot Count is \(carrotCount)")
+        
+        return carrotCount
     }
-
-    highestValues(dict)    
-  }
-  
-  func highestValues(_ dict : [Int: [Int]]) {
-    let sortedDict = dict.sorted { $0.0 < $1.0 }
-    let values = sortedDict.last!.value
-    print(values)
-    currentRow = values.first!
-    currentColumn = values.last!
-  }
-
-  func getCount() {
-    let startRows = findPossibleCenters(size: arr.count)
-    let startColumns = findPossibleCenters(size: arr[0].count)
-    determineCenterPosition(rows: startRows, columns: startColumns)
-    while !rabbitSleep{
-      eatCarrot()
-      possibleMoves()
-    }
-    print("Carrot Count is \(carrotCount)")
-  }
 }
 
- var arr = [
-    [5, 7, 8, 6, 3],
-    [0, 0, 7, 0, 4],
-    [4, 6, 3, 4, 9],
-    [3, 1, 0, 5, 8]
- ]
-  
-  
-var carrot = CarrotSearch(arr: arr)
-carrot.getCount()
-
-
-
+let matrix = [
+            [5, 7, 8, 6, 3],
+            [0, 0, 7, 0, 4],
+            [4, 6, 3, 4, 9],
+            [3, 1, 0, 5, 8]
+        ]
+        
+        let carrot = CarrotSearch(matrix: matrix)
+        let carrotCount = carrot.fetchCarrotCount()
+        print(carrotCount)
+ 
+ 
