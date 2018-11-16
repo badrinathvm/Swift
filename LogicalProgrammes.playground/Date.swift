89f3444df331108855dd45babc63e25f80536aef
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2018-11-16 08:55:07 -0800

    calculate the quality value

--- LogicalProgrammes.playground/Date.swift
+++ LogicalProgrammes.playground/Date.swift
@@ -9,7 +9,6 @@ func diffBetweenTwoDates(current: Date, expiration: Date) -> Int {
 
 let currentDate = Date()
 
-
 //construct future Date
 let stringDate = "2018-11-25"
 let dateFormatter = DateFormatter()
@@ -19,3 +18,45 @@ let futureDate = dateFormatter.date(from: stringDate)
 
 let noOfDays = diffBetweenTwoDates(current: dateA , expiration: futureDate!)
 print(noOfDays)
+
+
+enum Type {
+  case Thermometer, Bacteria, Flu
+}
+
+class Item {
+    var product: Type
+    var useIn: Int
+    var qualityValue:Int
+  
+   init(product:Type, useIn:Int, qualityValue: Int) {
+     self.product = product
+     self.useIn = useIn
+     self.qualityValue = qualityValue
+   }
+}
+
+var item1 = Item(product: .Bacteria, useIn: 10 , qualityValue: 20)
+//print(item1)
+
+func updateQuality(item: Item) -> Int {
+   switch item.product {
+     case .Thermometer: 
+            //No chnage in the quality
+            return  item.qualityValue
+     case .Bacteria:
+            let expirationDate = addDate(days: item.useIn)
+            let diff = diffBetweenTwoDates(current: Date() , expiration: expirationDate!)
+            if diff > 0 {
+              return item.qualityValue + 1
+            }else {
+              return item.qualityValue
+            }
+     case .Flu: 
+            print("Flu")
+           return 0
+   }
+}
+
+let quality = updateQuality(item : item1)
+print("Qualit is \(quality)")
