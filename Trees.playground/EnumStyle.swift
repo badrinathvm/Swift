4ccc49d418d0ada89a1c7c6e23f89515a41ab8d4
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2018-11-18 20:41:44 -0800

    Enum style of writing Binary Search Tree

---
+++ Trees.playground/EnumStyle.swift
@@ -0,0 +1,64 @@
+// This is a sandbox to experiment with CoderPad's execution capabilities.
+// It's a temporary, throw-away session only visible to you.
+
+import Foundation
+
+//Logic : 
+// 1.  indirect - Tells the compiler to insert the necessary layer of redirection especially for recursive calls.
+
+
+enum BinaryTree<Element: Comparable> {
+  case empty
+  indirect case node(Element, left:BinaryTree<Element>, right:BinaryTree<Element>)
+}
+
+extension BinaryTree {
+  func inserting(_ newElement: Element) -> BinaryTree<Element> {
+    switch self {
+      case .empty:
+            return .node(newElement, left: .empty, right: .empty)
+      case let .node(element, left, right):
+           if newElement > element {
+              return .node(element, left: left, right: right.inserting(newElement))
+           }else {
+             return .node(element, left: left.inserting(newElement), right: right)
+           }
+     }
+  }
+  
+  func contains(_ element : Element)  -> Bool{
+    switch self {
+      case .empty: return false
+      case let .node(root, left, right):
+        if root == element {
+          return true
+        }else if element > root {
+          return right.contains(element)
+        }else{
+          return left.contains(element)
+        }
+    }
+  }
+}
+
+extension BinaryTree: CustomStringConvertible {
+  var description: String {
+    switch self {
+      case .empty:
+         return "()"
+      case let .node(element, .empty, .empty):
+         return "(\(element))"
+      case let .node(element, left , right):
+         return "(\(left) \(element) \(right))"
+    }
+  }
+}
+
+var tree = BinaryTree<Int>.empty
+var t = tree.inserting(3).inserting(5).inserting(2)
+print(t)
+
+print(t.contains(3))
+print(t.contains(10))
+
+
