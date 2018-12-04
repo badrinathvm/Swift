eda0bdc664e861947b2826615b93cb81d863d5ad
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2018-12-03 17:03:37 -0800

    printPalindromePartitions

--- TopAlgorithms.swift
+++ TopAlgorithms.swift
@@ -30,3 +30,40 @@ func reverseSkipSpecialCharacters() {
 
 
 reverseSkipSpecialCharacters()
+
+
+//---------------------------------------------------------------- //
+
+func printPalindromePartitions() {
+  
+    let str = "nitin"
+    var finalResult = [String]()
+  
+    str.enumerated().forEach { i, val1 in 
+      for j in i+1..<str.count {
+        let start = str.index(str.startIndex, offsetBy: i)
+        let end = str.index(str.startIndex, offsetBy: j)
+
+        let subStr = String(str[start...end])
+
+        if subStr == String(subStr.reversed()) {
+           if subStr.count != str.count {
+              var tempArr = str.compactMap { $0 }
+              //encodedOffset gives the index value of the String.Index
+              tempArr.insert(" ", at: start.encodedOffset)
+              // Why end + 2 , +1 is since we inserted new character , and one more + 1 is to insert at the end.
+              tempArr.insert(" ", at: end.encodedOffset + 2)
+              let modifiedString = tempArr.reduce("" , { String($0) + String($1) })
+              finalResult.append(modifiedString)
+           }else{
+             finalResult.append(subStr)
+           }
+        }
+      }
+   }
+
+   print(finalResult)
+}
+
+
+printPalindromePartitions()  //["nitin", "n iti n"]
