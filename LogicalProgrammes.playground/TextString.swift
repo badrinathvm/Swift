638fde95a63a7bcf2667d9a29da0a61fe5646121
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2019-03-10 14:16:55 -0700

    Array Cyclic

--- LogicalProgrammes.playground/TextString.swift
+++ LogicalProgrammes.playground/TextString.swift
@@ -21,3 +21,46 @@ func textPatternSearch() {
 }
 
 textPatternSearch()
+
+
+// Array cyclic 
+
+//Logic : For every two increment of p increment q and check p == q if yes "cyclic" else "NO"
+// Make sure to check for bounday cases.
+
+func cyclic(arr: [Int]) -> Bool {
+  var p = 0
+  var q = 0
+  
+  while (true) {
+     
+    guard p >= 0 && q >= 0 && p <= arr.count && q <= arr.count else { return false }
+  
+    // increment p  ( once )
+    p = arr[p]
+    
+    print("Value of p first is \(p)")
+  
+    if p == q { 
+      print("Present") 
+      return true 
+    }
+  
+   guard p > 0 && p <= arr.count else { return false }
+  
+   //increment p (twice)
+   p = arr[p]
+    print("Value of p twice is \(p)")
+  
+   if p == q { return true }
+  
+   // Now check Q 
+   q = arr[q]
+    print("Value of q is \(q)")
+  
+   if p == q { return true }
+  }
+}
+
+let status = cyclic(arr : [1,2,4,3,4,8])
+print(status)
